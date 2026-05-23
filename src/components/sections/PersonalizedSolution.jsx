e1a786fd7f6f17430ac10d2a46bcfa5fe7cf2f11
Wajiha1122-source <wajihaazeem36@gmail.com>
2026-05-23 09:20:11 +0100

    scss resolved

--- src/components/sections/PersonalizedSolution.jsx
+++ src/components/sections/PersonalizedSolution.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { motion } from "framer-motion"
-import "../../../styles/components/personalSolution.scss"
+import "../../styles/components/personalSolution.scss"
 
 export default function PersonalizedSolution() {
 
